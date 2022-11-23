b1c5e07977bd956bc00aa701cf43ea54371db3c9
crocochon <chanakan.3869@gmail.com>
2022-11-23 07:39:19 +0700

    add howtoplay

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import 'phaser';
 import Phaser from 'phaser';
 import GameScene from './scenes/GameScene';
+import howtoplay from './scenes/howtoplay';
 
 
 const config = {
@@ -18,7 +19,8 @@ const config = {
         }
     },
     scene: [
-        GameScene
+        GameScene,
+        howtoplay
     ],
     
     
---
+++ src/scenes/howtoplay.js
@@ -0,0 +1,25 @@
+import Phaser from "phaser";
+
+
+
+class howtoplay extends Phaser.Scene {
+    constructor(test) {
+        super({
+            key: 'howtoplay'
+        });
+    }
+
+    preload() {
+        // code here
+
+    }
+
+    create() {
+        
+    }
+
+    update(delta, time) {
+        
+    }
+}
+export default howtoplay;
