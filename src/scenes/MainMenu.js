72ebc44d78e63c73a85b9b5f02ea7579bc865f9f
crocochon <chanakan.3869@gmail.com>
2022-11-25 02:59:23 +0700

    add

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -1,6 +1,6 @@
 import Phaser from "phaser";
 let bgmenu1 ;
-let bgmenu2 ;
+let Slime;
 
 
 
@@ -12,10 +12,10 @@ class MainMenu extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('bgmenu1','./src/scenes/image/backgrounds/png/bg04/Layers/Sky.png')
-        this.load.image('bgmenu2','./src/scenes/image/backgrounds/png/bg01/Layers/Ground.png')
+        this.load.image('bgmenu1','./src/scenes/image/backgrounds/png/bg04/Layers/Sky.png');
+        
 
-        this.load.spritesheet('slime','./src/scenes/image/Slimes/slimeMove/Bluemove.png')
+        this.load.spritesheet('slime','./src/scenes/image/Slimes/slimeMove/Bluemove.png',{frameWidth: 80, frameHeight: 54});
 
 
     }
@@ -23,14 +23,30 @@ class MainMenu extends Phaser.Scene {
     create() {
         //backgrounds
         bgmenu1 = this.add.image(630, 360, 'bgmenu1').setDepth(1);
-        bgmenu2 = this.add.image(630, 360, 'bgmenu2').setDepth(2); 
+       
+
+        //character
+        Slime = this.physics.add.sprite(150,650,'slime')
+        .setScale(3)
+        .setSize(25,20)
+        .setOffset(23,15)
+        .setDepth(5)
+        this.anims.create({
+           key: 'slimeAni',
+           frames: this.anims.generateFrameNumbers('slime', {
+            start: 0,
+            end: 6
+           }),
+           duration: 600 ,
+           repeat: -1
+        })
 
 
 
     }
 
     update(delta, time) {
-        
+        Slime.anims.play('slimeAni', true);
     }
 }
 export default MainMenu;
