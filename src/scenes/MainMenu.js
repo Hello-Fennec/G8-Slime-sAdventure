dc07fd83100a9a6b70f5273a8cb1f96e45f29ea5
crocochon <chanakan.3869@gmail.com>
2022-11-25 04:38:12 +0700

    add

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -3,6 +3,8 @@ let bg;
 let banner1;
 let banner2;
 let Frame;
+let Sign;
+let Obj;
 let Slime;
 let playButton;
 
@@ -20,7 +22,9 @@ class MainMenu extends Phaser.Scene {
         this.load.image('banner1','./src/scenes/image/Mainmenu/1.png')
         this.load.image('banner2','./src/scenes/image/Mainmenu/2.png')
         this.load.image('frame','./src/scenes/image/Mainmenu/11.png')
-        this.load.image('banner',./)
+        this.load.image('banner','/src/scenes/image/Mainmenu/sign.png')
+        this.load.image('obj1','./src/scenes/image/assests/png/Objects/Bush1.png')
+        this.load.image('play','./')
         this.load.spritesheet('slime','./src/scenes/image/Slimes/slimeMove/Bluemove.png',{frameWidth: 80, frameHeight: 54})
         
         
@@ -33,6 +37,8 @@ class MainMenu extends Phaser.Scene {
         banner1 = this.add.image(250,200, 'banner1').setScale(0.3).setDepth(0.2)
         banner2 = this.add.image(305,290, 'banner2').setScale(0.3).setDepth(0.2)
         Frame = this.add.image(305, 250, 'frame').setScale(0.29).setDepth(0.15)
+        Sign = this.add.image(950, 450,'banner').setScale(0.29).setDepth(0.2)
+        Obj = this.add.image(950, 600,'obj1').setScale(1).setDepth(0.2)
 
 
          
