f08373d67058162d098c58547ccc458278fc5c18
crocochon <chanakan.3869@gmail.com>
2022-11-25 05:24:26 +0700

    Update howtoplay.js

--- src/scenes/howtoplay.js
+++ src/scenes/howtoplay.js
@@ -1,6 +1,7 @@
 import Phaser from "phaser";
 let BackgroundHow;
 let SignBack;
+let BackButton;
 
 
 class howtoplay extends Phaser.Scene {
@@ -13,12 +14,26 @@ class howtoplay extends Phaser.Scene {
     preload() {
         this.load.image('bgHow','./src/scenes/image/Mainmenu/backGround.jpg')
         this.load.image('signback','./src/scenes/image/assests/png/Objects/ArrowSignLeft.png')
+        this.load.image('back','./src/scenes/image/Mainmenu/back.png')
 
     }
 
     create() {
         BackgroundHow = this.add.image(630, 360, 'bgHow').setDepth(0.1)
         SignBack = this.add.image(150, 600, 'signback').setScale(2.5).setDepth(0.12)
+        
+        //button
+        BackButton = this.add.image(170,570,'back').setScale(0.18).setDepth(0.13);
+        BackButton.setInteractive();
+        BackButton.on('pointerup',()=>{
+            this.scene.start('MainMenu');
+        })
+        BackButton.on('pointerover',()=>{
+            BackButton.setScale(0.22);
+        })
+        BackButton.on('pointerout',()=>{
+            BackButton.setScale(0.18);
+        })
     }
 
     update(delta, time) {
