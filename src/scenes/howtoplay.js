567f3fdd979879ce89b2c311be66cd7e032dd418
crocochon <chanakan.3869@gmail.com>
2022-11-25 05:07:57 +0700

    add

--- src/scenes/howtoplay.js
+++ src/scenes/howtoplay.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
-let backgroundHow;
+let BackgroundHow;
+let SignBack;
 
 
 class howtoplay extends Phaser.Scene {
@@ -11,11 +12,13 @@ class howtoplay extends Phaser.Scene {
 
     preload() {
         this.load.image('bgHow','./src/scenes/image/Mainmenu/backGround.jpg')
+        this.load.image('signback','./src/scenes/image/assests/png/Objects/ArrowSignLeft.png')
 
     }
 
     create() {
-        backgroundHow = this.add.image(630, 360, 'bgHow').setDepth(0.1)
+        BackgroundHow = this.add.image(630, 360, 'bgHow').setDepth(0.1)
+        SignBack = this.add.image(150, 600, 'signback').setScale(2.5).setDepth(0.12)
     }
 
     update(delta, time) {
